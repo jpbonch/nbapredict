4e90cad8892bc4bb88b23667e87ee17bd2c233d1
jpbonch <jp.bonchristiano@gmail.com>
2020-10-07 15:28:04 +0100

    Changed to 100 possesions

--- nbapredict.py
+++ nbapredict.py
@@ -1,3 +1,3 @@
 year = input('Year: ')
 
-url = 'https://stats.nba.com/teams/traditional/?sort=W_PCT&dir=-1&SeasonType=Regular%20Season&Season=' + year
+url = 'https://stats.nba.com/teams/traditional/?sort=W_PCT&dir=-1&SeasonType=Regular%20Season&Season=' + year + '&PerMode=Per100Possessions'
