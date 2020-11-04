1cb054d1bdab5a06b6a20e466f31f84c5a411799
jpbonch <jp.bonchristiano@gmail.com>
2020-11-04 17:31:53 +0000

    offensive rating algorithm

--- getstats.py
+++ getstats.py
@@ -1,35 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
 
-teamranges = {
-    'Milwaukee Bucks': 27
-
-}
-
-
 
 def scrapestats(url, team):
-
-    teamrange = teamranges[team]
-
     with open('NBA.com_Stats _ Teams Traditional.html', 'r', encoding='utf-8') as f:
         reading = f.read()
 
     soup = BeautifulSoup(reading, 'html.parser')
-    
+
     content = soup.html.select('td', {'target':'_blank'})
 
     stats=[]
+    rawstats=[]
     for i in range(840):
-            stats.append(content[i].text.strip('\n').strip(' '))
+            rawstats.append(content[i].text.strip('\n').strip(' '))
 
-    stats = [[stats[i]] for i in len(stats)]
-    print(stats)
+    for i in range(30):
+        stats.append([None]*28)
 
+    for teamy in range(30):
+        for value in range(28):
+            stats[teamy][value] = rawstats[0]
+            rawstats.pop(0)
 
+    for i in range(len(stats)):
+        if stats[i][1] == team:
+            row = stats[i]
 
+    row = [float(x) if row.index(x) != 1 else x for x in row]
 
-print(scrapestats('dumb', 'Milwaukee Bucks'))
+    statsdict = {}
+    statnames = ['seed', 'team', 'gp', 'w', 'l', 'win%', 'min', 'pts', 'fgm', 'fga',
+                'fg%', '3pm', '3pa', '3p%', 'ftm', 'fta', 'ft%', 'oreb', 'dreb',
+                'reb', 'ast', 'tov', 'stl', 'blk', 'blka', 'pf', 'pfd', '+/-']
+    for i in range(len(row)):
+        statsdict[statnames[i]] = row[i]
 
-
-    
+    return statsdict
--- nbapredict.py
+++ nbapredict.py
@@ -1,7 +1,12 @@
+# 2016 warriors: ((((1*114.8*42.7*49.5*11.9*38.3*17.6*78.8*44*30.1*9.5*6.7)/10)/20 + 11.5 * 10000000)/10**13)*(10/6.46469350606)
 year = input('Year: ')
 from getstats import scrapestats
 url = 'https://stats.nba.com/teams/traditional/?sort=W_PCT&dir=-1&SeasonType=Regular%20Season&Season=' + year + '&PerMode=Per100Possessions'
+team = 'Milwaukee Bucks'
+stats = scrapestats(url, team)
 
-staty = scrapestats(url)
+offensive = round(((stats['win%'] * stats['pts'] * stats['fgm'] * stats['fg%'] * stats['3pm'] *
+            stats['3p%'] * stats['ftm'] * stats['ft%'] * stats['reb'] * stats['ast'] *
+            stats['stl'] * stats['blk'])/stats['tov']) / 10**13, 2) * (100/60.91)
 
-print(staty)
+print(offensive)
