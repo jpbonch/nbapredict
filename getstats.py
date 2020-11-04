ae8e69073dd4b72bde8a79bf47bbda9d069fb3ac
tascottwatson <tascottwatson@hotmail.com>
2020-11-04 16:00:49 +0000

    changes

--- getstats.py
+++ getstats.py
@@ -1,15 +1,35 @@
 import requests
-from lxml import html
+from bs4 import BeautifulSoup
+
+teamranges = {
+    'Milwaukee Bucks': 27
+
+}
 
 
-def scrapestats(url):
-    page = requests.get(url)
-    root = html.fromstring(page.text)
-    tree = root.getroottree()
-    result = root.xpath('/html/body/main/div[2]/div/div[2]/div/div/nba-stat-table/div[2]/div[1]/table/tbody/tr[1]/td[6]')
 
-    return result
+def scrapestats(url, team):
+
+    teamrange = teamranges[team]
+
+    with open('NBA.com_Stats _ Teams Traditional.html', 'r', encoding='utf-8') as f:
+        reading = f.read()
+
+    soup = BeautifulSoup(reading, 'html.parser')
+    
+    content = soup.html.select('td', {'target':'_blank'})
+
+    stats=[]
+    for i in range(840):
+            stats.append(content[i].text.strip('\n').strip(' '))
+
+    stats = [[stats[i]] for i in len(stats)]
+    print(stats)
 
 
-    # stats = {'win': winpercentage}
-    # return statsx
+
+
+print(scrapestats('dumb', 'Milwaukee Bucks'))
+
+
+    
