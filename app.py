c3b7c018ea42e2f4a940869033fad5fc0f6d94d9
tricknac <jp.bonchristiano@bol.com.br>
2020-11-11 23:09:19 +0000

    Team stats lookup on index.html

--- app.py
+++ app.py
@@ -12,8 +12,21 @@ def index():
 
 @app.route("/form", methods=['GET', 'POST'])
 def form():
-    year = request.form.get('years')
-    
+    year = request.args.get('year')
+    team = request.args.get('team')
+    stats = scrapestats(team, year)
+
+    offensive = round(((stats['pts'] * stats['fgm'] * stats['fg%'] * stats['3pm'] *
+                stats['3p%'] * stats['ftm'] * stats['ft%'] * stats['oreb'] * stats['ast']) * (
+                100/42934758738941.51)), 2)
+
+    defensive = round((((stats['dreb'] *
+                stats['stl'] * stats['blk'])/stats['tov']) * (100/142)), 2)
+
+    total = round((offensive + defensive + stats['win%']*10) * 100/214.7, 2)
+
+    return render_template('index.html', offensive=offensive, defensive=defensive, total=total)
+
 
 
 if __name__ == "__main__":
