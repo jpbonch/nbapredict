825e6adcd55fd258ea57cd06bc0dd15ff0817b71
tascottwatson <47367118+tascottwatson@users.noreply.github.com>
2020-11-11 20:35:38 +0000

    added images to graph

--- plotting_results.py
+++ plotting_results.py
@@ -1,10 +1,23 @@
 #plots two things on a graph
+#pip install opencv-python
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import numpy as np
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from matplotlib.cbook import get_sample_data
 from getstats import scrapestats
+from PIL import Image
 
 year = input("Year: ")
 howmany = int(input("how many teams to plot: "))
+
+
+def getImage(path, zoom):
+    return OffsetImage(plt.imread(path, 0), zoom=zoom)
+
+offense = []
+defense = []
+paths = []
 
 for i in range(howmany):
 
@@ -26,20 +39,25 @@ for i in range(howmany):
     else:
         label = name[2]
 
-    img = mpimg.imread('bulls.png', 0)
+   
+    paths.append(f'Images/{label}.png')
+    offense.append(offensive)
+    defense.append(defensive)
 
-
-        
-    points = stats['pts']
-    rebounds = stats['dreb'] + stats['oreb']
-
-    plt.plot(offensive, defensive, 'ro')
-    plt.annotate(label, (offensive, defensive))
+    
 
 
 
+fig, ax = plt.subplots()
+ax.scatter(offense, defense) 
 
+for x0, y0, path in zip(offense, defense, paths):
+    ab = AnnotationBbox(getImage(path, zoom=0.3), (x0, y0), frameon=False)
+    ax.add_artist(ab)
+    
+plt.xlim(xmin=min(offense)-10, xmax=max(offense)+10)
+plt.ylim(ymin=min(defense)-10, ymax=max(defense)+10)
 plt.xlabel('Offensive')
-plt.ylabel('Defensive')    
+plt.ylabel('Defensive')
 plt.show()
 
