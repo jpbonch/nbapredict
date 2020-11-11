eee0d1fee023440a4544ea4885ff97fced56a574
tascottwatson <47367118+tascottwatson@users.noreply.github.com>
2020-11-11 20:38:23 +0000

    put it all in a function

--- plotting_results.py
+++ plotting_results.py
@@ -19,45 +19,48 @@ offense = []
 defense = []
 paths = []
 
-for i in range(howmany):
+def graphing():
 
-    team = input("Team: ")
-    stats = scrapestats(team, year)
+    for i in range(howmany):
 
-    offensive = round(((stats['pts'] * stats['fgm'] * stats['fg%'] * stats['3pm'] *
-            stats['3p%'] * stats['ftm'] * stats['ft%'] * stats['oreb'] * stats['ast']) * (
-            100/42934758738941.51)), 2)
+        team = input("Team: ")
+        stats = scrapestats(team, year)
 
-    defensive = round((((stats['dreb'] *
-            stats['stl'] * stats['blk'])/stats['tov']) * (100/142)), 2)
+        offensive = round(((stats['pts'] * stats['fgm'] * stats['fg%'] * stats['3pm'] *
+                stats['3p%'] * stats['ftm'] * stats['ft%'] * stats['oreb'] * stats['ast']) * (
+                100/42934758738941.51)), 2)
 
-    
+        defensive = round((((stats['dreb'] *
+                stats['stl'] * stats['blk'])/stats['tov']) * (100/142)), 2)
 
-    name = team.split(' ')
-    if len(name) == 2:
-        label = name[1]
-    else:
-        label = name[2]
+        
 
-   
-    paths.append(f'Images/{label}.png')
-    offense.append(offensive)
-    defense.append(defensive)
+        name = team.split(' ')
+        if len(name) == 2:
+            label = name[1]
+        else:
+            label = name[2]
 
     
+        paths.append(f'Images/{label}.png')
+        offense.append(offensive)
+        defense.append(defensive)
+
+        
 
 
 
-fig, ax = plt.subplots()
-ax.scatter(offense, defense) 
+    fig, ax = plt.subplots()
+    ax.scatter(offense, defense) 
 
-for x0, y0, path in zip(offense, defense, paths):
-    ab = AnnotationBbox(getImage(path, zoom=0.3), (x0, y0), frameon=False)
-    ax.add_artist(ab)
-    
-plt.xlim(xmin=min(offense)-10, xmax=max(offense)+10)
-plt.ylim(ymin=min(defense)-10, ymax=max(defense)+10)
-plt.xlabel('Offensive')
-plt.ylabel('Defensive')
-plt.show()
+    for x0, y0, path in zip(offense, defense, paths):
+        ab = AnnotationBbox(getImage(path, zoom=0.3), (x0, y0), frameon=False)
+        ax.add_artist(ab)
+        
+    plt.xlim(xmin=min(offense)-10, xmax=max(offense)+10)
+    plt.ylim(ymin=min(defense)-10, ymax=max(defense)+10)
+    plt.xlabel('Offensive')
+    plt.ylabel('Defensive')
+    plt.show()
 
+graphing()
