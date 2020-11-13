4d956633557e19981026450b32150c85d6dcfe3b
tascottwatson <47367118+tascottwatson@users.noreply.github.com>
2020-11-13 20:24:02 +0000

    added annotation on hover

--- plotting_playerstats.py
+++ plotting_playerstats.py
@@ -1,29 +1,43 @@
+#pip install mplcursors
 from getplayerstats import scrapeplayerstats
 import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+import mplcursors
 
 point = []
 reb = []
+players = []
+labels = []
 
-players = ["Joel Embiid", "James Harden", "Anthony Davis", "LeBron James", "Kawhi Leonard"]
-
-def plot(howmany):
+def plot():
     year = input("year: ")
+    file = 'data/playerdata/' + year + '.htm'
+    with open(file, 'r', encoding='utf-8') as f:
+            reading = f.read()
+    soup = BeautifulSoup(reading, 'html.parser')
+    content = soup.html.select("td", {'target' : '_blank'})
+
+    for i in range(1, 50):
+        players.append(content[(30 *(i-1)) + 1].text.strip('\n'))
+
     for name in players:
         stats = scrapeplayerstats(name, year)
         points = float(stats['pts'])
         rebounds = float(stats['oreb']) + float(stats['dreb'])
         point.append(points)
         reb.append(rebounds)
-        plt.annotate(name, (points,rebounds))
+        labels.append(name)
+
     
     plt.plot(point, reb, 'ob')
-    plt.xlim(xmin=min(point)-10, xmax=max(point) + 5)
-    plt.ylim(ymin=min(reb)-10, ymax=max(reb)+10)       
+    plt.xlim(xmin=min(point)-5, xmax=max(point) + 5)
+    plt.ylim(ymin=min(reb)-0.5, ymax=max(reb)+0.5)       
     plt.ylabel("Rebounds")
     plt.xlabel("Points")
+    mplcursors.cursor(hover=True).connect(
+        "add", lambda sel: sel.annotation.set_text(labels[sel.target.index]))
     plt.show()
 
-plot(5)
+ 
 
-
-    
+plot()
