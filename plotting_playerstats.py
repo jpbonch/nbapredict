f6887c1d74d32bbad5a5efdbb2ac61128117ea05
tascottwatson <tascottwatson@hotmail.com>
2020-11-18 09:48:39 +0000

    added some more detailed hover

--- plotting_playerstats.py
+++ plotting_playerstats.py
@@ -17,7 +17,7 @@ def plot(x, y):
     soup = BeautifulSoup(reading, 'html.parser')
     content = soup.html.select("td", {'target' : '_blank'})
 
-    for i in range(1, 10):
+    for i in range(1, 50):
         players.append(content[(30 *(i-1)) + 1].text.strip('\n'))
 
     for name in players:
@@ -42,4 +42,4 @@ def plot(x, y):
 
  
 
-plot("oreb", "dreb")
+plot("Age", "Points")
